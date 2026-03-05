2006f99355c541a039ab9b512061c681c349c429
Richard <rj.n@live.com>
2026-03-05 11:28:45 +0000

    sample product list

--- src/main/java/com/coffeeshop/menu/controller/ProductController.java
+++ src/main/java/com/coffeeshop/menu/controller/ProductController.java
@@ -1,10 +1,20 @@
 package com.coffeeshop.menu.controller;
 
+import com.coffeeshop.menu.model.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping
+import java.util.List;
 
+@Controller
+@RequestMapping("/products")// means all URL starts with  http://localhost:8080/products/
 public class ProductController {
+
+    private List<Product> productList= List.of(
+            new Product(1, "Espresso", 2.50),
+            new Product(2, "Latte", 3.50),
+            new Product(3, "Croissant", 2.00),
+            new Product(4, "Chocolate Muffin", 2.25),
+            new Product(5, "Americano", 2.75)
+    );
 }
